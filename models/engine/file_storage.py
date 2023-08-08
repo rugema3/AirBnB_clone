3968e689e3b87894cadc26c01d74d8d210826c2c
Jobs-rw <mjabush@yahoo.fr>
2023-08-08 04:27:23 -0700

    debug2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-from base_model import BaseModel
 import Json
-class FileStorage(BaseModel):
-    def __init__(self, __file_path, __objects):
-        super().__init(args, kwargs)
+class FileStorage:
+    def __init__(self):
         self.__file_path = 'file.json'
         self.__objects = {}
+
     def all(self):
-        return FileStorage.self.__objects
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                from base_model import BaseModel  # Assuming the BaseModel class exists
+                for key, obj_data in data.items():
+                    class_name, obj_id = key.split('.')
+                    class_ = eval(class_name)  # Convert class name to actual class
+                    obj = class_(**obj_data)
+                    self.__objects[key] = obj
 
