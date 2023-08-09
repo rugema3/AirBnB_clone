9b23ec4db4e319fd78c2699b36f8c5596fc63131
Jobs-rw <mjabush@yahoo.fr>
2023-08-09 11:31:03 -0700

    debug9

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+"""define user class"""
+class User(BaseModel):
+    """define user description"""
+    email =""
+    password =""
+    first_name =""
+    last_name =""
